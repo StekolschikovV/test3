2e09464ddb56f0dbc216fe88142c02e567f49ff2
mk <v.a.stekolschikov@gmail.com>
2022-05-09 15:22:12 +0300

    2

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const { PORT = 3000 } = process.env
 
 console.log('PORT', PORT);
 
-app.get('/', (req, res) => res.send('Hello world'))
+app.get('/', (req, res) => res.send('Hello world!!!'))
 
 app.listen(PORT, () => {
     console.log('Listening at ' + PORT);
