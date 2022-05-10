449473613a57d1c6e8eacc6f43432924e9f1cf83
Vetal <v.a.stekolschikov@gmail.com>
2022-05-10 19:02:10 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
-// const { PORT = 3000 } = process.env
-const PORT = 3000
+const { PORT = 3000 } = process.env
+//const PORT = 3000
 
 console.log('PORT', PORT);
 
